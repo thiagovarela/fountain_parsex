61e2cd9663e5bcd1df62b3e318a003dcc2a43524
thiagovarela <thiagovarela@gmail.com>
2025-08-18 10:37:59 +0100

    locally publishing

--- mix.exs
+++ mix.exs
@@ -3,11 +3,14 @@ defmodule FountainParsex.MixProject do
 
   def project do
     [
+      name: "FountainParsex",
       app: :fountain_parsex,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,9 +24,19 @@ defmodule FountainParsex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.38", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "A fountain screenplay parser"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/thiagovarela/fountain_parsex"}
     ]
   end
 end
